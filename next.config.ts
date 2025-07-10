72c5cd3f02d3f86cabffe8e532a7ca16f449650b
Dw33pY <dwaynemwaniki3@gmail.com>
2025-07-10 15:00:30 +0300

    tryin to fix error on launch

--- next.config.ts
+++ next.config.ts
@@ -5,4 +5,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
+module.exports = {
+  output: 'export',
+  basePath: '/smartfood-restaurant', // MUST match repo name
+  images: { unoptimized: true }
+}
+
 export default nextConfig;
